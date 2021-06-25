5427ec72755c229210b3ef1f63fad67fc719abfb
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2021-06-25 11:09:28 -0700

    Update bugScrub.py

--- bugScrub.py
+++ bugScrub.py
@@ -1,4 +1,3 @@
-import ipaddress
 import json
 import requests
 import ssl
@@ -10,6 +9,7 @@ cvp_user = 'admin'
 cvp_pass = 'Arista123'
 tokenFile = 'token.txt'
 certFile = 'cert.crt'
+
 def login(url_prefix, username, password):
     connect_timeout = 10
     headers = {"Accept": "application/json",
@@ -19,10 +19,11 @@ def login(url_prefix, username, password):
     response = session.post('https://'+url_prefix+'/web/login/authenticate.do', data=json.dumps(authdata),
                             headers=headers, timeout=connect_timeout,
                             verify=False)
+    inventoryOutput = session.get('https://'+url_prefix+'/cvpservice/inventory/devices')#?provisioned=true')
     if response.json()['sessionId']:
         token = response.json()['sessionId']
         sslcert = ssl.get_server_certificate((url_prefix,8443))
-        return [token,sslcert]
+        return [token,sslcert,inventoryOutput.json()]
 
 def getBugs(client):
     pathElts = [
@@ -61,6 +62,7 @@ def getBugDetails(client,bugID):
 def main(server=None, token=None, certs=None, ca=None, key=None):
     for server in cvp_servers:
         creds = login(server, cvp_user, cvp_pass)
+        inventory = creds[2]
         with open(tokenFile,"w") as f:
             f.write(creds[0])
             f.close()
@@ -75,23 +77,49 @@ def main(server=None, token=None, certs=None, ca=None, key=None):
                 specificBug = getBugDetails(client,bugID)
                 TotalBugs.append({bugID:specificBug})
         for bugtemp in TotalBugs:
-            bugnumber = list(bugtemp)
-            bug = bugtemp[bugnumber[0]]
-            print('===========================================================')
-            print('Bug ID: '+str(bugnumber[0]))
-            print('Severity: '+str(bug['severity']))
-            print('version Introduced: '+str(bug['versionIntroduced']))
-            print('version Fixed: '+str(bug['versionFixed']))
-            affectedSwitches = output[bugnumber[0]]
-            ##### To-Do, figure out hostname lookup failure.
-            #for serialNumber in affectedSwitches:
-            #    hostname = getHostname(client,serialNumber)
-            #    print(hostname)
-            #    #impactedDeviceList.append(hostname)
-            #impactedDeviceList = []
-            #print('Affected Devices: '+str(impactedDeviceList))
-            print('Affected Devices: '+str(affectedSwitches))
-            print('Summary: '+str(bug['alertSummary']))
+                bugnumber = list(bugtemp)
+                bug = bugtemp[bugnumber[0]]
+                if bug['cve']:
+                    print('===========================================================')
+                    print('Bug Type: CVE')
+                    print('Bug ID: '+str(bugnumber[0]))
+                    print('CVE ID: '+str(bug['cve']))
+                    print('Severity: '+str(bug['severity']))
+                    print('version Introduced: '+str(bug['versionIntroduced']))
+                    print('version Fixed: '+str(bug['versionFixed']))
+                    affectedSwitches = output[bugnumber[0]]
+                    hostlist = []
+                    for switch in affectedSwitches:
+                        for device in inventory:
+                            if device['serialNumber'] == switch:
+                                devHostname = device['hostname']
+                                devVersion = device['version']
+                                hostlist.append({devHostname:devVersion})
+                    print('Affected Switches: '+str(hostlist))
+                    print('Summary: '+str(bug['alertSummary']))
+                    print('CVP IP: '+server)
+                else:
+                    pass
+        for bugtemp in TotalBugs:
+                bugnumber = list(bugtemp)
+                bug = bugtemp[bugnumber[0]]
+                print('===========================================================')
+                print('Bug Type: BUG')
+                print('Bug ID: '+str(bugnumber[0]))
+                print('Severity: '+str(bug['severity']))
+                print('version Introduced: '+str(bug['versionIntroduced']))
+                print('version Fixed: '+str(bug['versionFixed']))
+                affectedSwitches = output[bugnumber[0]]
+                hostlist = []
+                for switch in affectedSwitches:
+                    for device in inventory:
+                        if device['serialNumber'] == switch:
+                            devHostname = device['hostname']
+                            devVersion = device['version']
+                            hostlist.append({devHostname:devVersion})
+                print('Affected Switches: '+str(hostlist))
+                print('Summary: '+str(bug['alertSummary']))
+                print('CVP IP: '+server)
 
 
 def grpc_query(client, query):
